c55cef136f0694840c9809d15e063abeb09a62c5
LeandroLeyes <Leandroleyes60@gmail.com>
2025-11-02 18:48:44 -0300

    Actualizacion carrousel.js
    Prototipo final

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,77 +1,59 @@
 
-const imagenesCarrousel1 = [
-    {
-        img1: './images/carrousel/avengerseg.jpg', //0
-        img2: './images/carrousel/avengersiw.img.jpg',//1
-        img3: './images/carrousel/bastardos.jpg'//2
-    },
-    {
-        img1: './images/carrousel/terminator.jpg',//8
-        img2: './images/carrousel/avengerseg.jpg',//0
-        img3: './images/carrousel/avengersiw.img.jpg'//1
-    },
-    {
-        img1: './images/carrousel/predator.jpg',//7
-        img2: './images/carrousel/terminator.jpg',//8
-        img3: './images/carrousel/avengerseg.jpg'//0
-    },
-    {
-        img1: './images/carrousel/lotr.jpg',//6
-        img2: './images/carrousel/predator.jpg',//7
-        img3: './images/carrousel/terminator.jpg'//8
-    },
-    {
-        img1: './images/carrousel/got.jpg',//5
-        img2: './images/carrousel/lotr.jpg',//6
-        img3: './images/carrousel/predator.jpg'//7
-    },
-    {
-        img1: './images/carrousel/fallout.jpg',//4
-        img2: './images/carrousel/got.jpg',//5
-        img3: './images/carrousel/lotr.jpg'//6
-    },
-    {
-        img1: './images/carrousel/breakingbad.jpg',//3
-        img2: './images/carrousel/fallout.jpg',//4
-        img3: './images/carrousel/got.jpg'//5
-    },
-    {
-        img1: './images/carrousel/bastardos.jpg',//2
-        img2: './images/carrousel/breakingbad.jpg',//3
-        img3: './images/carrousel/fallout.jpg'//4
-    },
-    {
-        img1: './images/carrousel/avengersiw.img.jpg',//1
-        img2: './images/carrousel/bastardos.jpg',//2
-        img3: './images/carrousel/breakingbad.jpg'//3
-    }
+const imagenesCarrousel = [
+    './images/carrousel/avengerseg.jpg',//0
+    './images/carrousel/avengersiw.img.jpg',//1
+    './images/carrousel/bastardos.jpg',//2
+    './images/carrousel/breakingbad.jpg',//3
+    './images/carrousel/fallout.jpg',//4
+    './images/carrousel/got.jpg',//5
+    './images/carrousel/lotr.jpg',//6
+    './images/carrousel/predator.jpg',//7
+    './images/carrousel/terminator.jpg'//8
 ]
 
 let imagenP1 = document.getElementById('imagen1')
 let imagenP2 = document.getElementById('imagen2')
 let imagenP3 = document.getElementById('imagen3')
 
-let i = 0
+let i = 1
+let max = imagenesCarrousel.length - 1
 
 function mostrarImagenes() {
-    let aux = imagenesCarrousel1[i]
-    imagenP1.src = aux.img1
-    imagenP2.src = aux.img2
-    imagenP3.src = aux.img3
+    imagenP1.src = imagenesCarrousel[i - 1]
+    imagenP2.src = imagenesCarrousel[i]
+    imagenP3.src = imagenesCarrousel[i + 1]
 }
 
-function atras() {
+function adelante() {
     i++
-    if (i >= imagenesCarrousel1.length) {
+    if (i == max - 1) {
+        imagenP1.src = imagenesCarrousel[i]
+        imagenP2.src = imagenesCarrousel[max]
+        imagenP3.src = imagenesCarrousel[0]
+        return
+    } else if (i == max) {
         i = 0
+        imagenP1.src = imagenesCarrousel[max]
+        imagenP2.src = imagenesCarrousel[i]
+        imagenP3.src = imagenesCarrousel[1]
+        return
     }
     mostrarImagenes()
 }
 
-function adelante() {
+function atras() {
     i--
-    if (i < 0) {
-        i = imagenesCarrousel1.length - 1
+    if (i <= 0) {
+        i = max
+        imagenP1.src = imagenesCarrousel[i]
+        imagenP2.src = imagenesCarrousel[0]
+        imagenP3.src = imagenesCarrousel[1]
+        return
+    } else if (i == max - 1) {
+        imagenP1.src = imagenesCarrousel[i]
+        imagenP2.src = imagenesCarrousel[max]
+        imagenP3.src = imagenesCarrousel[0]
+        return
     }
     mostrarImagenes()
 }
