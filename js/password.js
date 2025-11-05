2f3298af5242b81088ea726dbeadc7d829246140
AzulDevLaHoz <azuldelahoz@gmail.com>
2025-11-05 17:30:51 -0300

    Flex de secciones Peliculas, Series, Estrenos

--- js/password.js
+++ js/password.js
@@ -2,12 +2,12 @@ let password1 = document.getElementById('contraseña')
 let password2 = document.getElementById('contraseña2')
 
 function verPassword() {
-    if (password1.getAttribute.type == 'password' && password2.getAttribute.type == 'password') {
-        password1.setAttribute.type = 'text'
-        password2.setAttribute.type = 'text'
+    if (password1.type == 'password' && password2.type == 'password') {
+        password1.type = 'text'
+        password2.type = 'text'
 
     } else {
-        password1.setAttribute.type = 'password'
-        password2.setAttribute.type = 'password'
-    }
+        password1.type = 'password'
+        password2.type = 'password'
+    }
 }
